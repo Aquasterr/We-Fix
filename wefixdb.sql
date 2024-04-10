922aea9f2345c744a3177dce863aed8bc4a88ee6
Aquaster <110834755+Aquasterr@users.noreply.github.com>
2024-04-10 20:34:56 -0300

    Update wefixdb.sql

--- wefixdb.sql
+++ wefixdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Abr-2024 às 14:20
--- Versão do servidor: 10.3.16-MariaDB
--- versão do PHP: 7.3.6
+-- Tempo de geração: 11/04/2024 às 01:34
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `arquiteto`
+-- Estrutura para tabela `arquiteto`
 --
 
 CREATE TABLE `arquiteto` (
@@ -39,20 +38,20 @@ CREATE TABLE `arquiteto` (
   `id_cau_arquiteto` int(11) NOT NULL,
   `local` varchar(255) NOT NULL,
   `nota` decimal(2,1) NOT NULL DEFAULT 5.0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Extraindo dados da tabela `arquiteto`
+-- Despejando dados para a tabela `arquiteto`
 --
 
 INSERT INTO `arquiteto` (`nome`, `cpf_arquiteto`, `email`, `senha`, `endereco`, `certificados`, `telefone`, `id_cau_arquiteto`, `local`, `nota`) VALUES
-('Lara Isis Galvão', '37600419734', 'lara-galvao80@mpcnet.com.br', 'lara123', 'Travessa do Piquizeiro', '', '9936376747', 2147483647, 'São Paulo - SP', '5.0'),
-('Arquiteto Nome 2', '78978978978', 'Arquiteto2@email.com', 'Arquiteto123', 'Rua Arquiteto2', 'pipipipipopopo', '456456456', 123123123, 'São Caetano do Sul - SP', '5.0');
+('Lara Isis Galvão', '37600419734', 'lara-galvao80@mpcnet.com.br', 'lara123', 'Travessa do Piquizeiro', '', '9936376747', 2147483647, 'São Paulo - SP', 5.0),
+('Arquiteto Nome 2', '78978978978', 'Arquiteto2@email.com', 'Arquiteto123', 'Rua Arquiteto2', 'pipipipipopopo', '456456456', 123123123, 'São Caetano do Sul - SP', 5.0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cliente`
+-- Estrutura para tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -63,10 +62,10 @@ CREATE TABLE `cliente` (
   `endereco` varchar(255) NOT NULL,
   `telefone` varchar(15) NOT NULL,
   `local` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Extraindo dados da tabela `cliente`
+-- Despejando dados para a tabela `cliente`
 --
 
 INSERT INTO `cliente` (`nome_cliente`, `cpf_cliente`, `email`, `senha`, `endereco`, `telefone`, `local`) VALUES
@@ -77,19 +76,19 @@ INSERT INTO `cliente` (`nome_cliente`, `cpf_cliente`, `email`, `senha`, `enderec
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `empreiteiro`
+-- Estrutura para tabela `empreiteiro`
 --
 
 CREATE TABLE `empreiteiro` (
   `idEmpreiteiro` int(11) NOT NULL,
   `cpfArquiteto` varchar(11) NOT NULL,
   `cpfEngenheiro` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `engenheiro`
+-- Estrutura para tabela `engenheiro`
 --
 
 CREATE TABLE `engenheiro` (
@@ -103,20 +102,20 @@ CREATE TABLE `engenheiro` (
   `id_crea_engenheiro` int(11) NOT NULL,
   `local` varchar(255) NOT NULL,
   `nota` decimal(2,1) NOT NULL DEFAULT 5.0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Extraindo dados da tabela `engenheiro`
+-- Despejando dados para a tabela `engenheiro`
 --
 
 INSERT INTO `engenheiro` (`nome`, `cpf_engenheiro`, `email`, `senha`, `endereco`, `certificados`, `telefone`, `id_crea_engenheiro`, `local`, `nota`) VALUES
-('Fábio Edson Anderson Nunes', '142100262', 'fabioedsonnunes@effem.com', 'fabio123', 'Avenida Guanabara', '', '6936033452', 2147483647, 'São Paulo - SP', '5.0'),
-('Engenheiro', '98798798778', 'EngenheiroTF2@Engenheiro', 'Engenheiro123', 'Rua Engenheiro Estrada Engenheiro', 'C:\\fakepath\\Android Large - 1.png', '456456456', 2147483647, 'São Paulo - SP', '5.0');
+('Fábio Edson Anderson Nunes', '142100262', 'fabioedsonnunes@effem.com', 'fabio123', 'Avenida Guanabara', '', '6936033452', 2147483647, 'São Paulo - SP', 5.0),
+('Engenheiro', '98798798778', 'EngenheiroTF2@Engenheiro', 'Engenheiro123', 'Rua Engenheiro Estrada Engenheiro', 'C:\\fakepath\\Android Large - 1.png', '456456456', 2147483647, 'São Paulo - SP', 5.0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `equipe`
+-- Estrutura para tabela `equipe`
 --
 
 CREATE TABLE `equipe` (
@@ -125,12 +124,12 @@ CREATE TABLE `equipe` (
   `senha` varchar(100) NOT NULL,
   `quantidade_equipe` int(11) NOT NULL,
   `especialidades` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `obra`
+-- Estrutura para tabela `obra`
 --
 
 CREATE TABLE `obra` (
@@ -145,20 +144,23 @@ CREATE TABLE `obra` (
   `cpf_profissional` varchar(255) DEFAULT NULL,
   `cpf_cliente` varchar(255) NOT NULL,
   `status` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Extraindo dados da tabela `obra`
+-- Despejando dados para a tabela `obra`
 --
 
 INSERT INTO `obra` (`id_obra`, `local_obra`, `descricao_obra`, `data_inicio`, `data_termino`, `avaliacao_obra`, `cpf_arquiteto`, `cpf_engenheiro`, `cpf_profissional`, `cpf_cliente`, `status`) VALUES
-(123, 'São Paulo - SP', 'e uma obra', '2024-04-02', '2024-04-30', '5.0', '78978978978', '98798798778', '45645678978', '45685245612', ''),
-(821487, 'São Caetano do Sul - SP', 'asdasdsad', '2322-12-31', '1111-11-11', NULL, NULL, NULL, NULL, '45685245612', '');
+(123, 'São Paulo - SP', 'e uma obra', '2024-04-02', '2024-04-30', 5.0, '78978978978', '98798798778', '45645678978', '45685245612', 'PENDENTE'),
+(11111, 'São Paulo - SP', 'Descricao Descricao', '2024-04-12', '2024-08-15', NULL, NULL, NULL, '31238949088', '45685245612', 'EM ANDAMENTO'),
+(22222, 'São Paulo - SP', 'Teste Obra Finalizada Teste', '2024-04-26', '2024-12-05', 4.5, NULL, '142100262', NULL, '45685245612', 'FINALIZADAS'),
+(528047, 'São Caetano do Sul - SP', 'Salve Josefa', '2024-04-12', '2024-04-30', NULL, NULL, NULL, '40833523880', '45685245612', 'PENDENTE'),
+(821487, 'São Caetano do Sul - SP', 'asdasdsad', '2322-12-31', '1111-11-11', NULL, '37600419734', NULL, NULL, '45685245612', 'PENDENTE');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `profissionais_solicitados`
+-- Estrutura para tabela `profissionais_solicitados`
 --
 
 CREATE TABLE `profissionais_solicitados` (
@@ -167,12 +169,12 @@ CREATE TABLE `profissionais_solicitados` (
   `cpf_arquiteto` varchar(11) NOT NULL,
   `cpf_engenheiro` varchar(11) NOT NULL,
   `cpf_profissional` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `profissionalgeral`
+-- Estrutura para tabela `profissionalgeral`
 --
 
 CREATE TABLE `profissionalgeral` (
@@ -186,23 +188,23 @@ CREATE TABLE `profissionalgeral` (
   `telefone` varchar(10) NOT NULL,
   `local` varchar(255) NOT NULL,
   `nota` decimal(2,1) NOT NULL DEFAULT 5.0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Extraindo dados da tabela `profissionalgeral`
+-- Despejando dados para a tabela `profissionalgeral`
 --
 
 INSERT INTO `profissionalgeral` (`nome`, `cpf_profissional`, `email`, `senha`, `endereco`, `especialidade`, `certificados`, `telefone`, `local`, `nota`) VALUES
-('André Pedro Henrique Rafael Farias', '31238949088', 'andre-farias72@macroengenharia.com', 'andre123', 'Residencial Solar da Chapada', 'Pintor', '', '6599946899', 'São Caetano do Sul - SP', '5.0'),
-('Josefa Liz Nogueira', '40833523880', 'josefa-nogueira97@imeio.com', 'josefa123', 'Passagem Santo Antônio', 'Mestre de Obra', '', '9699805621', 'São Paulo - SP', '5.0'),
-('Profissional Teste 2', '45645678978', 'ProfissionalTeste2@mail.com', 'profissional123', 'Rua Profissional Teste 2', 'Mestre de Obra', NULL, '9845625685', 'São Paulo - SP', '5.0'),
-('Marcos Vinicius Marcelo Almada', '78599340271', 'marcos-almada95@viamoc.com.br', 'marcos123', 'Rua Marechal Deodoro', 'Eletricista', '', '5599787956', 'São Paulo - SP', '5.0'),
-('Geraldo Victor Lopes', '96335953048', 'geraldo_lopes@hotmail.con', 'geraldo123', 'Rua Homero Viana', 'Encanador', '', '5125024398', 'São Caetano do Sul - SP', '5.0');
+('André Pedro Henrique Rafael Farias', '31238949088', 'andre-farias72@macroengenharia.com', 'andre123', 'Residencial Solar da Chapada', 'Pintor', '', '6599946899', 'São Caetano do Sul - SP', 5.0),
+('Josefa Liz Nogueira', '40833523880', 'josefa-nogueira97@imeio.com', 'josefa123', 'Passagem Santo Antônio', 'Mestre de Obra', '', '9699805621', 'São Paulo - SP', 5.0),
+('Profissional Teste 2', '45645678978', 'ProfissionalTeste2@mail.com', 'profissional123', 'Rua Profissional Teste 2', 'Mestre de Obra', NULL, '9845625685', 'São Paulo - SP', 5.0),
+('Marcos Vinicius Marcelo Almada', '78599340271', 'marcos-almada95@viamoc.com.br', 'marcos123', 'Rua Marechal Deodoro', 'Eletricista', '', '5599787956', 'São Paulo - SP', 5.0),
+('Geraldo Victor Lopes', '96335953048', 'geraldo_lopes@hotmail.con', 'geraldo123', 'Rua Homero Viana', 'Encanador', '', '5125024398', 'São Caetano do Sul - SP', 5.0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `solicitacao`
+-- Estrutura para tabela `solicitacao`
 --
 
 CREATE TABLE `solicitacao` (
@@ -212,27 +214,36 @@ CREATE TABLE `solicitacao` (
   `data_solicitacao` datetime NOT NULL,
   `nome_cliente` varchar(100) DEFAULT NULL,
   `cpf_cliente` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Despejando dados para a tabela `solicitacao`
+--
+
+INSERT INTO `solicitacao` (`id_solicitacao`, `local`, `descricao`, `data_solicitacao`, `nome_cliente`, `cpf_cliente`) VALUES
+(201458, 'São Caetano do Sul - SP', 'Solicitação Teste2Solicitação Teste2Solicitação Teste2Solicitação Teste2', '2024-04-10 13:11:37', 'ClienteTeste', '45685245612'),
+(469958, 'São Paulo - SP', 'Solicitação Teste', '2024-04-10 13:11:21', 'ClienteTeste', '45685245612'),
+(875111, 'São Paulo - SP', 'Solicitação Teste', '2024-04-09 22:26:47', 'ClienteTeste', '45685245612');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `arquiteto`
+-- Índices de tabela `arquiteto`
 --
 ALTER TABLE `arquiteto`
   ADD PRIMARY KEY (`cpf_arquiteto`);
 
 --
--- Índices para tabela `cliente`
+-- Índices de tabela `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`cpf_cliente`),
   ADD UNIQUE KEY `nome_cliente` (`nome_cliente`);
 
 --
--- Índices para tabela `empreiteiro`
+-- Índices de tabela `empreiteiro`
 --
 ALTER TABLE `empreiteiro`
   ADD PRIMARY KEY (`idEmpreiteiro`),
@@ -240,19 +251,19 @@ ALTER TABLE `empreiteiro`
   ADD KEY `cpfEngenheiro` (`cpfEngenheiro`);
 
 --
--- Índices para tabela `engenheiro`
+-- Índices de tabela `engenheiro`
 --
 ALTER TABLE `engenheiro`
   ADD PRIMARY KEY (`cpf_engenheiro`);
 
 --
--- Índices para tabela `equipe`
+-- Índices de tabela `equipe`
 --
 ALTER TABLE `equipe`
   ADD PRIMARY KEY (`email_equipe`);
 
 --
--- Índices para tabela `obra`
+-- Índices de tabela `obra`
 --
 ALTER TABLE `obra`
   ADD PRIMARY KEY (`id_obra`),
@@ -262,7 +273,7 @@ ALTER TABLE `obra`
   ADD KEY `cpf_cliente` (`cpf_cliente`);
 
 --
--- Índices para tabela `profissionais_solicitados`
+-- Índices de tabela `profissionais_solicitados`
 --
 ALTER TABLE `profissionais_solicitados`
   ADD PRIMARY KEY (`id_profissional`),
@@ -272,13 +283,13 @@ ALTER TABLE `profissionais_solicitados`
   ADD KEY `cpf_profissional` (`cpf_profissional`);
 
 --
--- Índices para tabela `profissionalgeral`
+-- Índices de tabela `profissionalgeral`
 --
 ALTER TABLE `profissionalgeral`
   ADD PRIMARY KEY (`cpf_profissional`);
 
 --
--- Índices para tabela `solicitacao`
+-- Índices de tabela `solicitacao`
 --
 ALTER TABLE `solicitacao`
   ADD PRIMARY KEY (`id_solicitacao`),
@@ -286,18 +297,18 @@ ALTER TABLE `solicitacao`
   ADD KEY `fk_nome_cliente` (`nome_cliente`);
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `empreiteiro`
+-- Restrições para tabelas `empreiteiro`
 --
 ALTER TABLE `empreiteiro`
   ADD CONSTRAINT `empreiteiro_ibfk_1` FOREIGN KEY (`cpfArquiteto`) REFERENCES `arquiteto` (`cpf_arquiteto`),
   ADD CONSTRAINT `empreiteiro_ibfk_2` FOREIGN KEY (`cpfEngenheiro`) REFERENCES `engenheiro` (`cpf_engenheiro`);
 
 --
--- Limitadores para a tabela `obra`
+-- Restrições para tabelas `obra`
 --
 ALTER TABLE `obra`
   ADD CONSTRAINT `obra_ibfk_1` FOREIGN KEY (`cpf_arquiteto`) REFERENCES `arquiteto` (`cpf_arquiteto`),
@@ -306,7 +317,7 @@ ALTER TABLE `obra`
   ADD CONSTRAINT `obra_ibfk_4` FOREIGN KEY (`cpf_cliente`) REFERENCES `cliente` (`cpf_cliente`);
 
 --
--- Limitadores para a tabela `profissionais_solicitados`
+-- Restrições para tabelas `profissionais_solicitados`
 --
 ALTER TABLE `profissionais_solicitados`
   ADD CONSTRAINT `profissionais_solicitados_ibfk_5` FOREIGN KEY (`id_solicitacao`) REFERENCES `solicitacao` (`id_solicitacao`),
@@ -315,7 +326,7 @@ ALTER TABLE `profissionais_solicitados`
   ADD CONSTRAINT `profissionais_solicitados_ibfk_8` FOREIGN KEY (`cpf_profissional`) REFERENCES `profissionalgeral` (`cpf_profissional`);
 
 --
--- Limitadores para a tabela `solicitacao`
+-- Restrições para tabelas `solicitacao`
 --
 ALTER TABLE `solicitacao`
   ADD CONSTRAINT `fk_cpf_cliente` FOREIGN KEY (`cpf_cliente`) REFERENCES `cliente` (`cpf_cliente`),
