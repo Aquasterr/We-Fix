7f8a979c747d28d2fbbc5523a47d62bc9e0bf352
Henrique Willian <110834755+Aquasterr@users.noreply.github.com>
2024-05-14 09:51:12 -0300

    Update Wefix_Server.js

--- Wefix_Server.js
+++ Wefix_Server.js
@@ -455,6 +455,6 @@ app.get('/', (request, response) => {
 })
 
 // Printa Mensagem ao conectar com a porta
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log("Servidor conectado na porta " + port)
 })
